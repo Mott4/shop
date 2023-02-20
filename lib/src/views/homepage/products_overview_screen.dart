ea9ce7ff07ed8220e6269c7c27b2effee2cabe75
Vitor Mota <dailyslokoscontatoofc@gmail.com>
2023-02-20 14:51:24 -0300

    task: refatoração

--- lib/src/views/homepage/products_overview_screen.dart
+++ lib/src/views/homepage/products_overview_screen.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:shop/src/models/product_list.dart';
 import 'package:shop/src/views/homepage/components/product_grid.dart';
-import 'package:shop/src/views/homepage/components/product_item.dart';
-import 'package:shop/src/models/product.dart';
 
 class ProductsOverviewScreen extends StatelessWidget {
   ProductsOverviewScreen({super.key});
