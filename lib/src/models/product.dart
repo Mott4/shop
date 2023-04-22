2d1c40b318fce70951c4cb288f98fad744eccf59
Vitor Mota <dailyslokoscontatoofc@gmail.com>
2023-04-22 20:38:45 -0300

    feat:(Product-List): Updating product - isfavorite

--- lib/src/models/product.dart
+++ lib/src/models/product.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 import 'package:flutter/material.dart';
+import 'package:shop/src/constants/constants.dart';
+import 'package:shop/src/exceptions/http_exception.dart';
 
 class Product with ChangeNotifier {
   final String id;
@@ -19,8 +24,25 @@ class Product with ChangeNotifier {
 
 // Methods
 
-  void toggleFavorite() {
+  void _toggleFavorite() {
     isFavorite = !isFavorite!;
     notifyListeners();
+  }
+
+  Future<void> toggleFavorite() async {
+    try {
+      _toggleFavorite();
+
+      final response = await http.patch(
+        Uri.parse('${Constants.products_baseUrl}/$id.json'),
+        body: jsonEncode({"isFavorite": isFavorite}),
+      );
+
+      if (response.statusCode >= 400) {
+        _toggleFavorite();
+      }
+    } catch (_) {
+      _toggleFavorite();
+    }
   }
 }
