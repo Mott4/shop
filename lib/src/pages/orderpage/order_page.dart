eedfb68ccfbb4ac35aad759557a7dcb68ed14ea0
Vitor Mota <dailyslokoscontatoofc@gmail.com>
2023-04-24 09:06:42 -0300

    fixe:(Orders): FutureBuilder added

--- lib/src/pages/orderpage/order_page.dart
+++ lib/src/pages/orderpage/order_page.dart
@@ -9,8 +9,6 @@ class OrdersPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    //
-
     final OrderList orders = Provider.of(context);
 
     return Scaffold(
@@ -18,7 +16,22 @@ class OrdersPage extends StatelessWidget {
         title: const Text('Meus Pedidos'),
       ),
       drawer: const AppDrawer(),
-      body: ListView.builder(itemCount: orders.itemsCount, itemBuilder: (ctx, i) => OrderWidget(order: orders.items[i])),
+      body: FutureBuilder(
+          builder: (ctx, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (snapshot.error != null) {
+              return const Center(child: Text('Ocorreu um erro!'));
+            } else {
+              return Consumer<OrderList>(
+                builder: (ctx, orders, child) => ListView.builder(
+                  itemCount: orders.itemsCount,
+                  itemBuilder: (ctx, i) => OrderWidget(order: orders.items[i]),
+                ),
+              );
+            }
+          },
+          future: Provider.of<OrderList>(context, listen: false).loadOrders()),
     );
   }
 }
